1543a8d61fc60752f66d52a85efdc40c98512815
Graeme Hollingsworth <ghollingsworth@LBMX.COM>
2023-02-16 14:24:05 -0500

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [entry, setValue] = useState("");
+  const [shoppingListValues, setShoppingValue] = useState<string[]>([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Grocery Store Order:</h2>
+        {shoppingListValues.map((item) => (
+          <div className="horizontal-flow store-item" key={item.toString()}>
+            <div>
+              {item}{" "}
+              <button
+                className="remove-button"
+                onClick={(x) => removeButtonClicked(item.toString())}
+              >
+                X
+              </button>
+            </div>
+          </div>
+        ))}
+        <br />
+        <input
+          type="text"
+          value={entry}
+          onChange={(x) => setValue(x.target.value)}
+          onKeyDown={textBoxKeyPress}
+        />
+        <div className="horizontal-flow">
+          <button onClick={addToShoppingList}>Submit</button>
+          <button onClick={clearShoppingList}>Clear</button>
+        </div>
       </header>
     </div>
   );
+
+  function textBoxKeyPress(e: any) {
+    if (e.key === "Enter" && entry !== "") {
+      addToShoppingList();
+    }
+  }
+
+  function removeButtonClicked(key: string) {
+    setShoppingValue(shoppingListValues.filter((item) => item !== key));
+  }
+
+  function addToShoppingList() {
+    setValue("");
+    setShoppingValue([...shoppingListValues, entry]);
+  }
+
+  function clearShoppingList() {
+    setShoppingValue([]);
+  }
 }
 
 export default App;
